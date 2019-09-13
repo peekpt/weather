d5c14ece33482c9455b3f954b93205157f2617af
Paulo <paulo@iMac.home>
2019-09-13 14:41:46 +0100

    wip

---
+++ lib/widgets/location_preview_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LocationPreviewWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
